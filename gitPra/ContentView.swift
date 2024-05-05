f8e55cdafa5c044ce227e1eb5a0e251fa9fb01f9
LET <bjs.6300.137@gmail.com>
2024-05-05 20:16:51 +0900

    Initial Commit

---
+++ gitPra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitPra
+//
+//  Created by 津本拓也 on 2024/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitPra/gitPraApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPraApp.swift
+//  gitPra
+//
+//  Created by 津本拓也 on 2024/05/05.
+//
+
+import SwiftUI
+
+@main
+struct gitPraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
